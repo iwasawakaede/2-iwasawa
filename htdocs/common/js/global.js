e9892421a17a1bf82cfe51e908755bd03960d2b0
iwasawa <fyanze93@gmail.com>
2023-08-22 15:56:15 +0900

    線位置調整

--- htdocs/common/js/global.js
+++ htdocs/common/js/global.js
@@ -1,3 +1,8 @@
 $(function () {
-
+	alert('OK!');
+	console.log("start");
+	$(".l-headerButtonOpenbtn").click(function () {//ボタンがクリックされたら
+		$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+		$(".l-headerBlock").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+	});
 });

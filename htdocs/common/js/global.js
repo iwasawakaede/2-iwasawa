d72e1f880dedc94b7f0b4180ba401168851f5b57
iwasawa <fyanze93@gmail.com>
2023-08-22 18:13:39 +0900

    メニュー調整

--- htdocs/common/js/global.js
+++ htdocs/common/js/global.js
@@ -1,8 +1,8 @@
 $(function () {
-	alert('OK!');
+	// alert('OK!');
 	console.log("start");
 	$(".l-headerButtonOpenbtn").click(function () {//ボタンがクリックされたら
 		$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-		$(".l-headerBlock").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+		$(".l-headerNav").toggleClass('is-panelactive');//ナビゲーションにpanelactiveクラスを付与
 	});
 });

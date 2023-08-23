8608c89dfe77726d9d297c40205735a21422c064
iwasawa <fyanze93@gmail.com>
2023-08-23 12:58:14 +0900

    .fixed削除　hiddenに変更

--- htdocs/common/js/global.js
+++ htdocs/common/js/global.js
@@ -1,8 +1,9 @@
 $(function () {
-	// alert('OK!');
+	alert('OK!');
 	console.log("start");
 	$(".l-headerButtonOpenbtn").click(function () {//ボタンがクリックされたら
 		$(this).toggleClass('is-active');//ボタン自身に activeクラスを付与し
 		$(".l-headerNav").toggleClass('is-panelactive');//ナビゲーションにpanelactiveクラスを付与
+		$("body").toggleClass('fixed');//bodyにfixedクラスを付与
 	});
 });

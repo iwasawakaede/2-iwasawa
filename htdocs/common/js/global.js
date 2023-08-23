156b3fc710f87ebccd722577b2282b88dc8845b0
iwasawa <fyanze93@gmail.com>
2023-08-23 14:01:17 +0900

    menu内のサイズ調整

--- htdocs/common/js/global.js
+++ htdocs/common/js/global.js
@@ -1,5 +1,5 @@
 $(function () {
-	alert('OK!');
+	// alert('OK!');
 	console.log("start");
 	$(".l-headerButtonOpenbtn").click(function () {//ボタンがクリックされたら
 		$(this).toggleClass('is-active');//ボタン自身に activeクラスを付与し

82e30dd124bbc681592165122c488ef7dc37dbab
iwasawa <fyanze93@gmail.com>
2023-08-24 11:00:02 +0900

    js追加

--- htdocs/common/js/global.js
+++ htdocs/common/js/global.js
@@ -6,4 +6,32 @@ $(function () {
 		$(".l-headerNav").toggleClass('is-panelactive');//ナビゲーションにpanelactiveクラスを付与
 		$("body").toggleClass('fixed');//bodyにfixedクラスを付与
 	});
+
+	var beforePos = 0;//スクロールの値の比較用の設定
+
+//スクロール途中でヘッダーが消え、上にスクロールすると復活する設定を関数にまとめる
+function ScrollAnime() {
+    var elemTop = $('#area-2').offset().top;//#area-2の位置まできたら
+	var scroll = $(window).scrollTop();
+    //ヘッダーの出し入れをする
+    if(scroll == beforePos) {
+		//IE11対策で処理を入れない
+    }else if(elemTop > scroll || 0 > scroll - beforePos){
+		//ヘッダーが上から出現する
+		$('#header').removeClass('UpMove');	//#headerにUpMoveというクラス名を除き
+		$('#header').addClass('DownMove');//#headerにDownMoveのクラス名を追加
+    }else {
+		//ヘッダーが上に消える
+        $('#header').removeClass('DownMove');//#headerにDownMoveというクラス名を除き
+		$('#header').addClass('UpMove');//#headerにUpMoveのクラス名を追加
+    }
+    
+    beforePos = scroll;//現在のスクロール値を比較用のbeforePosに格納
+}
+
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	ScrollAnime();//スクロール途中でヘッダーが消え、上にスクロールすると復活する関数を呼ぶ
+});
 });

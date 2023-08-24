6472e613f119925c46c25a39b2c15f7f9d86d82a
iwasawa <fyanze93@gmail.com>
2023-08-24 12:58:41 +0900

    js理解中…

--- htdocs/common/js/global.js
+++ htdocs/common/js/global.js
@@ -9,38 +9,37 @@ $(function () {
 
 	var beforePos = 0;//スクロールの値の比較用の設定
 
-//スクロール途中でヘッダーが消え、上にスクロールすると復活する設定を関数にまとめる
+//２→（１でスクロールすると呼び出される）スクロール途中でヘッダーが消え、上にスクロールすると復活する設定を関数にまとめる
 function ScrollAnime() {
-	console.log("1");
-    var elemTop = $('.l-contents').offset().top;//.l-contentsSampleの垂直（縦）位置を取得したら
-		console.log("2");
+	//console.log("1");
+    var elemTop = $('.l-contents').offset().top;//.l-contentsのスクロール位置を取得する
+		 console.log(elemTop);
 	var scroll = $(window).scrollTop();
-	console.log("3");
+	console.log('これはスクロールの値',scroll);
     //ヘッダーの出し入れをする（画面のスクロール位置を取得する）
-    if(scroll == beforePos) {
-			console.log("4");
-		//IE11対策で処理を入れない
-    }else if(elemTop > scroll || 0 > scroll - beforePos){
-			console.log("5");
+		console.log('beforePosの値',beforePos)
+// ３→現在のスクロール値がl-contentsのスクロール値より大きいと？（分岐点）
+if(elemTop > scroll || 0 > scrollPos){
+			// console.log("5");
 		//ヘッダーが上から出現する
 		$('.l-headerTopJs').removeClass('.UpMove');	//.l-headerTopJsにUpMoveというクラス名を除き
-		console.log("6");
+		// console.log("6");
 		$('.l-headerTopJs').addClass('.DownMove');//.l-headerTopJsにDownMoveのクラス名を追加
-		console.log("7");
+		// console.log("7");
     }else {
 		//ヘッダーが上に消える
         $('.l-headerTopJs').removeClass('.DownMove');//.l-headerTopJsにDownMoveというクラス名を除き(下に下がる動き)
-				console.log("8");
+				// console.log("8");
 		$('.l-headerTopJs').addClass('.UpMove');//.l-headerTopJsにUpMoveのクラス名を追加(上に上がる動き)
-		console.log("9");
+		// console.log("9");
     }
     
     beforePos = scroll;//現在のスクロール値を比較用のbeforePosに格納
 }
 
 
-// 画面をスクロールをしたら動かしたい場合の記述
+// 1→マウスを1回スクロールすると、2の関数を呼び出す（）
 $(window).scroll(function () {
-	ScrollAnime();//スクロール途中でヘッダーが消え、上にスクロールすると復活する関数を呼ぶ
+	ScrollAnime();
 });
 });

589d3f6719bfe21ca8e3471c6f24b14b2049e9f3
iwasawa <fyanze93@gmail.com>
2023-09-26 12:58:42 +0900

    写真選択部分CSS作成途中

--- htdocs/common/js/global.js
+++ htdocs/common/js/global.js
@@ -92,4 +92,12 @@ checkBreakPoint();
 			}
 		});
 
+	//fileを変更したら
+$('input').on('change', function () {
+	//propを使って、file[0]にアクセスする
+	var file = $(this).prop('files')[0];
+	//text()で要素内のテキストを変更する
+	$('.select-image').text(file.name);
+});
+
 });

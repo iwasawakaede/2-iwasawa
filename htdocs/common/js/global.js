eba9f16c9a94aa7e055fb2a9b1795f3979802c87
iwasawakaede <fyanze93@gmail.com>
2023-09-27 16:03:33 +0900

    モーダルウィンドウ作成途中

--- htdocs/common/js/global.js
+++ htdocs/common/js/global.js
@@ -100,4 +100,18 @@ $('input').on('change', function () {
 	$('.select-image').text(file.name);
 });
 
+const modal = $(".js-modal");
+const overlay = $(".js-overlay");
+const close = $(".js-close");
+const open = $(".js-open");
+
+open.on('click', function () { //ボタンをクリックしたら
+  modal.addClass("open"); // modalクラスにopenクラス付与
+  overlay.addClass("open"); // overlayクラスにopenクラス付与
+});
+close.on('click', function () { //×ボタンをクリックしたら
+  modal.removeClass("open"); // overlayクラスからopenクラスを外す
+  overlay.removeClass("open"); // overlayクラスからopenクラスを外す
+});
+
 });
